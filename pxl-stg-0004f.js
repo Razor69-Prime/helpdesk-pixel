e009f9adb73c3020ced1b1c6c71e5e725e8c98fd
Razor69-Prime <ekahendrayana1986@gmail.com>
2026-08-01 12:36:58 +0800

    PXL-STG-0005H: refresh consolidated frontend asset

--- pxl-stg-0004f.js
+++ pxl-stg-0004f.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// PXL-STG-0005G menjadi satu-satunya konsolidator flow baru.
+// PXL-STG-0005H menjadi satu-satunya konsolidator flow baru.
 require('./pxl-stg-0005d');
 
 const fs = require('fs');
@@ -8,11 +8,11 @@ const path = require('path');
 const express = require('express');
 const originalStatic = express.static;
 
-express.static = function pxl0005gStatic(root, options) {
+express.static = function pxl0005hStatic(root, options) {
   const middleware = originalStatic(root, options);
   const indexPath = path.join(root, 'index.html');
   const salesOrderPath = path.join(root, 'sales-order.html');
-  return function pxl0005gMiddleware(req, res, next) {
+  return function pxl0005hMiddleware(req, res, next) {
     const isIndex = req.method === 'GET' && (req.path === '/' || req.path === '/index.html') && fs.existsSync(indexPath);
     const isSalesOrder = req.method === 'GET' && req.path === '/sales-order.html' && fs.existsSync(salesOrderPath);
     if (!isIndex && !isSalesOrder) return middleware(req, res, next);
@@ -21,8 +21,8 @@ express.static = function pxl0005gStatic(root, options) {
       if (isIndex) {
         const tags = [
           '<script src="/pxl-stg-0004d.js?v=PXL-STG-0004D"></script>',
-          '<script src="/pxl-stg-0004f.js?v=PXL-STG-0005G"></script>',
-          '<script src="/pxl-stg-0005d.js?v=PXL-STG-0005G"></script>'
+          '<script src="/pxl-stg-0004f.js?v=PXL-STG-0005H"></script>',
+          '<script src="/pxl-stg-0005d.js?v=PXL-STG-0005H"></script>'
         ];
         for (const tag of tags) {
           const src = tag.match(/src="([^"]+)/)?.[1];
