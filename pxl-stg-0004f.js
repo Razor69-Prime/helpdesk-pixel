a8d95faa0ff72734238b3bc516d90f99909578ca
Razor69-Prime <ekahendrayana1986@gmail.com>
2026-08-25 14:04:03 +0800

    PXL-URG-0021D remove duplicate Sales Order feature loader

--- pxl-stg-0004f.js
+++ pxl-stg-0004f.js
@@ -29,16 +29,11 @@ function replaceOrAppendScript(html,base,version){const src=`${base}?v=${version
 function forceSalesOrderScriptOrder(html){
   const bases=['/pxl-stg-0006c-sales-order.js','/pxl-stg-0006k-polish.js','/pxl-stg-0006l-pdf-fix.js','/pxl-urg-0014-sales-order-wo-date.js','/pxl-urg-0014a-sales-order-wo-date-direct.js','/pxl-urg-0019-native-wo-date.js','/pxl-urg-0021-sales-order-manual-material-maps.js'];
   for(const base of bases)html=removeScript(html,base);
-  const pxl0021Path=path.join(__dirname,'public','pxl-urg-0021-sales-order-manual-material-maps.js');
-  const pxl0021Inline=fs.existsSync(pxl0021Path)
-    ? `<script data-pxl-urg-0021a="inline">\n${fs.readFileSync(pxl0021Path,'utf8')}\n</script>`
-    : '<script src="/pxl-urg-0021-sales-order-manual-material-maps.js?v=PXL-URG-0021A"></script>';
   const ordered=[
     '<script src="/pxl-stg-0006c-sales-order.js?v=PXL-STG-0006N"></script>',
     '<script src="/pxl-stg-0006k-polish.js?v=PXL-PROD-0022PDF5"></script>',
     '<script src="/pxl-stg-0006l-pdf-fix.js?v=PXL-PROD-0022PDF5"></script>',
-    '<script src="/pxl-urg-0019-native-wo-date.js?v=PXL-URG-0019"></script>',
-    pxl0021Inline
+    '<script src="/pxl-urg-0019-native-wo-date.js?v=PXL-URG-0019"></script>'
   ].join('\n');
   return html.replace('</body>',ordered+'\n</body>');
 }
